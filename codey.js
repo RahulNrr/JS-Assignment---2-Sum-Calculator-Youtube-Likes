334a85198ada4c738799506ae0552f4c832c1810
Rahul Traksh Nagar <66189513+RahulNrr@users.noreply.github.com>
2021-10-15 19:06:34 +0530

    Add files via upload

---
+++ codey.js
@@ -0,0 +1,11 @@
+let x = 0;
+function increment(){
+    x = x+1;
+    document.getElementById('counter').innerHTML = x;
+}
+function decrement(){
+    if(x>0){
+        x = x-1;
+        document.getElementById('counter').innerHTML = x;
+    }
+}
